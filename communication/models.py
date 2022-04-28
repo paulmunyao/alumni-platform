e4ae9eb1ad5f73222b888d78979037606f82c4ea
Paul Munyao <paulmunyao094@gmail.com>
2022-04-28 08:59:11 +0300

    Create class profile in the models class

--- communication/models.py
+++ communication/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    bio = models.TextField(max_length=500, blank=True)
+    def __str__(self):
+        return self.user.username
