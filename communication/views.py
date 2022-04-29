0aa42c6072c7f227e901c04515aa5babff44cd5a
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-29 17:07:22 +0300

    create view function for profile

--- communication/views.py
+++ communication/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
+def index(request):
+    return render(request,'home.html')
+
+
+def profile_update(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile= Profile.objects.filter(username=current_user)
+            print(profile)
+            if profile:
+                print('profile exist')
+                username = current_user
+                useremail=form.cleaned_data['useremail']
+               
+                userage=form.cleaned_data['userage']
+                profile_image=form.cleaned_data['profile_image']
+                AuthenticationError=form.cleaned_data['AuthenticationError']
+                Profile.objects.filter(username=current_user).update(useremail=useremail, userage=userage,profile_image=profile_image,AuthenticationError=AuthenticationError)
+            else:
+                print('profile does not exist')
+                profile=form.save(commit=False)
+                profile.username= current_user
+                profile.save()
+
+            message='saved successfuly'
+            # profile_display(request)
+            return redirect(profile_display)
+    
+            
+    else:
+        form = ProfileForm()
+        
+    return render(request, 'updateprofile.html',{'form':form})
+
+def profile_display(request):
+
+    current_user = request.user
+    profile= Profile.objects.filter(username_id=request.user.id)
+   
+
+    return render(request, 'profile.html',{"profiles":profile})
