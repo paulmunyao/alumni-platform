4a5f1a5719b43258284e0089caf655628f977b24
Paul Munyao <paulmunyao094@gmail.com>
2022-04-30 05:42:48 +0300

    Set configuration keys

--- communication/models.py
+++ communication/models.py
@@ -10,3 +10,26 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    message = models.TextField(max_length=500, blank=True)
+    def __str__(self):
+        return self.user.username   
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    members = models.ManyToManyField(User, related_name='groups')
+    def __str__(self):
+        return self.name 
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField(max_length=500, blank=True)
+    def __str__(self):
+        return self.user.username
+        
+                            
