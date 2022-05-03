e3e454c617c7127d90d5e9104abfb15404aae07a
Paul Munyao <paulmunyao094@gmail.com>
2022-05-03 05:18:00 +0300

    Update models file so that when a new post or message is made it shows the time

--- communication/models.py
+++ communication/models.py
@@ -13,8 +13,7 @@ class Profile(models.Model):
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    message = models.TextField(max_length=500, blank=True)
+    message = models.TextField(auto_now=True, max_length=500, blank=True)
     def __str__(self):
         return self.user.username   
 
@@ -28,8 +27,8 @@ class Group(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    message = models.TextField(max_length=500, blank=True)
+    message = models.TextField(auto_now=True, max_length=500, blank=True)
     def __str__(self):
         return self.user.username
         
-                            
+                        
