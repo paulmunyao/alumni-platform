8a93256309af822eda0bc3d07b5da3bbac824c99
Paul Munyao <paulmunyao094@gmail.com>
2022-05-03 05:21:11 +0300

    Update comment and post model to show the correct timestamp

--- communication/models.py
+++ communication/models.py
@@ -14,6 +14,8 @@ class Profile(models.Model):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(auto_now=True, max_length=500, blank=True)
+    updated = models.DateTimeField(auto_now=True)   
+    timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.user.username   
 
@@ -28,6 +30,8 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField(auto_now=True, max_length=500, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.user.username
         
