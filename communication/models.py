8d4381b7da24bf5f4aa1f16a9aa92e151c75f737
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-05-03 15:34:47 +0300

    create profile model

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ImageField
+
+
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.OneToOneField(User,on_delete=models.CASCADE,default=0,related_name='profile')
+    bio = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='images/')
+    useremail = models.EmailField(max_length=20)
+    
