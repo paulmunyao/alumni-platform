220d120bb7bfb423d4b704f9f1a815ed6d561114
Paul Munyao <paulmunyao094@gmail.com>
2022-05-04 04:53:57 +0300

    Create endpoints for different model

--- communication/views.py
+++ communication/views.py
@@ -5,3 +5,28 @@ from .models import Profile, Post, Group, Comment
 from .serializers import ProfileSerializer, PostSerializer, GroupSerializer, CommentSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_profiles(request):
+
+    routes = [
+        {'Endpoint': '/profiles/', 
+        'method': 'GET', 
+        'body': 'None',
+        'description': 'Get all profiles'},
+
+        {'Endpoint': '/posts/',
+        'method': 'GET',
+        'body': 'None',
+        'description': 'Get all posts'},
+
+        {'Endpoint': '/groups/',
+        'method': 'GET',
+        'body': 'None',
+        'description': 'Get all groups'},
+
+        {'Endpoint': '/comments/',
+        'method': 'GET',
+        'body': 'None',
+        'description': 'Get all comments'},
+    ]
