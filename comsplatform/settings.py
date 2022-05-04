421083e8c0a3cbe4abb2591ed0ebb162ca55278f
Paul Munyao <paulmunyao094@gmail.com>
2022-05-04 06:08:28 +0300

    Settings configurations for private keys

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -15,12 +15,13 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from pathlib import Path
+import dj_database_url
 from decouple import config, Csv
 
 cloudinary.config(
-    CLOUD_NAME = config('CLOUD_NAME'),
-    API_KEY = config('API_KEY'),
-    API_SECRET = config('API_SECRET')
+    CLOUD_NAME=config('CLOUD_NAME'),
+    API_KEY=config('API_KEY'),
+    API_SECRET=config('API_SECRET')
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,12 +90,33 @@ WSGI_APPLICATION = 'comsplatform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+MODE = config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+if config('MODE') == "dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
+
     }
-}
+
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
