e129fa77a8811cfd48dd807b4f781cd8be131888
Paul Munyao <paulmunyao094@gmail.com>
2022-05-04 04:17:20 +0300

    Update configurations

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -16,11 +16,12 @@ import cloudinary.uploader
 import cloudinary.api
 from pathlib import Path
 
-# cloudinary.config(
-#     CLOUD_NAME = config('CLOUD_NAME'),
-#     API_KEY = config('API_KEY'),
-#     API_SECRET = config('API_SECRET')
-# )
+
+cloudinary.config(
+    CLOUD_NAME = config('CLOUD_NAME'),
+    API_KEY = config('API_KEY'),
+    API_SECRET = config('API_SECRET')
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
