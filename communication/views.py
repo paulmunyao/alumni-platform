f497a34ff422bbdbeaf55319c23644424ecc5070
Paul Munyao <paulmunyao094@gmail.com>
2022-05-04 04:44:53 +0300

    Import serializers to views

--- communication/views.py
+++ communication/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Profile, Post, Group, Comment
+from .serializers import ProfileSerializer, PostSerializer, GroupSerializer, CommentSerializer
 
 # Create your views here.
