4edb05a5c48fe4ed1b906a056a2b7c9e52d0fee7
Paul Munyao <paulmunyao094@gmail.com>
2022-05-05 18:34:32 +0300

    Deployment

--- communication/views.py
+++ communication/views.py
@@ -6,120 +6,124 @@ from .serializers import ProfileSerializer, PostSerializer, GroupSerializer, Com
 
 # Create your views here.
 
+
 @api_view(['GET'])
 def getRoutes(request):
 
     routes = [
         # creating endpoints for getting all the items
-        {'Endpoint': '/profiles/', 
-        'method': 'GET', 
-        'body': 'None',
-        'description': 'Get all profiles'},
+        {'Endpoint': 'api/profiles/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get all profiles'},
 
-        {'Endpoint': '/posts/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get all posts'},
+        {'Endpoint': 'api/posts/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get all posts'},
 
-        {'Endpoint': '/groups/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get all groups'},
+        {'Endpoint': 'api/groups/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get all groups'},
         #    print('groups')
 
-        {'Endpoint': '/comments/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get all comments'},
+        {'Endpoint': 'api/comments/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get all comments'},
 
         # Endpoints for getting a single item
-        {'Endpoint': '/profiles/<int:pk>/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get a profile'},
+        {'Endpoint': 'api/profiles/<int:pk>/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get a profile'},
 
-        {'Endpoint': '/posts/<int:pk>/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get a post'},
+        {'Endpoint': 'api/posts/<int:pk>/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get a post'},
 
-        {'Endpoint': '/groups/<int:pk>/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get a group'},
+        {'Endpoint': 'api/groups/<int:pk>/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get a group'},
 
-        {'Endpoint': '/comments/<int:pk>/',
-        'method': 'GET',
-        'body': 'None',
-        'description': 'Get a comment'},
+        {'Endpoint': 'api/comments/<int:pk>/',
+         'method': 'GET',
+         'body': 'None',
+         'description': 'Get a comment'},
 
         # Endpoints for deleting a single item
-        {'Endpoint': '/profiles/<int:pk>/delete/',
-        'method': 'DELETE',
-        'body': 'None',
-        'description': 'Delete a profile'},
+        {'Endpoint': 'api/profiles/<int:pk>/delete/',
+         'method': 'DELETE',
+         'body': 'None',
+         'description': 'Delete a profile'},
 
-        {'Endpoint': '/posts/<int:pk>/delete/',
-        'method': 'DELETE',
-        'body': 'None',
-        'description': 'Delete a post'},
+        {'Endpoint': 'api/posts/<int:pk>/delete/',
+         'method': 'DELETE',
+         'body': 'None',
+         'description': 'Delete a post'},
 
-        {'Endpoint': '/groups/<int:pk>/delete/',
-        'method': 'DELETE',
-        'body': 'None',
-        'description': 'Delete a group'},
+        {'Endpoint': 'api/groups/<int:pk>/delete/',
+         'method': 'DELETE',
+         'body': 'None',
+         'description': 'Delete a group'},
 
-        {'Endpoint': '/comments/<int:pk>/delete/',
-        'method': 'DELETE',
-        'body': 'None',
-        'description': 'Delete a comment'},
+        {'Endpoint': 'api/comments/<int:pk>/delete/',
+         'method': 'DELETE',
+         'body': 'None',
+         'description': 'Delete a comment'},
 
         # Endpoints for updating a single item
-        {'Endpoint': '/profiles/<int:pk>/update/',
-        'method': 'PUT',
-        'body': 'ProfileSerializer',
-        'description': 'Update a profile'},
+        {'Endpoint': 'api/profiles/<int:pk>/update/',
+         'method': 'PUT',
+         'body': 'ProfileSerializer',
+         'description': 'Update a profile'},
 
-        {'Endpoint': '/posts/<int:pk>/update/',
-        'method': 'PUT',
-        'body': 'PostSerializer',
-        'description': 'Update a post'},
+        {'Endpoint': 'api/posts/<int:pk>/update/',
+         'method': 'PUT',
+         'body': 'PostSerializer',
+         'description': 'Update a post'},
 
-        {'Endpoint': '/groups/<int:pk>/update/',
-        'method': 'PUT',
-        'body': 'GroupSerializer',
-        'description': 'Update a group'},
+        {'Endpoint': 'api/groups/<int:pk>/update/',
+         'method': 'PUT',
+         'body': 'GroupSerializer',
+         'description': 'Update a group'},
 
-        {'Endpoint': '/comments/<int:pk>/update/',
-        'method': 'PUT',
-        'body': 'CommentSerializer',
-        'description': 'Update a comment'},
+        {'Endpoint': 'api/comments/<int:pk>/update/',
+         'method': 'PUT',
+         'body': 'CommentSerializer',
+         'description': 'Update a comment'},
 
         # Endpoints for creating a single item
-        {'Endpoint': '/posts/<int:pk>/create/',
-        'method': 'POST',
-        'body': 'PostSerializer',
-        'description': 'Create a post'},
+        {'Endpoint': 'api/posts/<int:pk>/create/',
+         'method': 'POST',
+         'body': 'PostSerializer',
+         'description': 'Create a post'},
 
-        {'Endpoint': '/groups/<int:pk>/create/',
-        'method': 'POST',
-        'body': 'GroupSerializer',
-        'description': 'Create a group'},
+        {'Endpoint': 'api/groups/<int:pk>/create/',
+         'method': 'POST',
+         'body': 'GroupSerializer',
+         'description': 'Create a group'},
 
-        {'Endpoint': '/comments/<int:pk>/create/',
-        'method': 'POST',
-        'body': 'CommentSerializer',
-        'description': 'Create a comment'},
+        {'Endpoint': 'api/comments/<int:pk>/create/',
+         'method': 'POST',
+         'body': 'CommentSerializer',
+         'description': 'Create a comment'},
     ]
 
     return Response(routes)
 
-#getting an item
+# getting an item
+
+
 @api_view(['GET'])
 def get_profiles(request):
     profiles = Profile.objects.all()
     serializer = ProfileSerializer(profiles, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_posts(request):
@@ -127,11 +131,13 @@ def get_posts(request):
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def get_groups(request):
     groups = Group.objects.all()
     serializer = GroupSerializer(groups, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_comments(request):
@@ -139,12 +145,15 @@ def get_comments(request):
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data)
 
-#getting a single item
+# getting a single item
+
+
 @api_view(['GET'])
 def get_profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     serializer = ProfileSerializer(profile)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_post(request, pk):
@@ -152,11 +161,13 @@ def get_post(request, pk):
     serializer = PostSerializer(post)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def get_group(request, pk):
     group = Group.objects.get(pk=pk)
     serializer = GroupSerializer(group)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_comment(request, pk):
@@ -164,12 +175,15 @@ def get_comment(request, pk):
     serializer = CommentSerializer(comment)
     return Response(serializer.data)
 
-#deleting a single item
+# deleting a single item
+
+
 @api_view(['DELETE'])
 def delete_profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     profile.delete()
     return Response('Profile deleted successfully!')
+
 
 @api_view(['DELETE'])
 def delete_post(request, pk):
@@ -177,11 +191,13 @@ def delete_post(request, pk):
     post.delete()
     return Response('Post deleted successfully!')
 
+
 @api_view(['DELETE'])
 def delete_group(request, pk):
     group = Group.objects.get(pk=pk)
     group.delete()
     return Response('Group deleted successfully!')
+
 
 @api_view(['DELETE'])
 def delete_comment(request, pk):
@@ -189,15 +205,19 @@ def delete_comment(request, pk):
     comment.delete()
     return Response('Comment deleted successfully!')
 
-#Updating an item
+# Updating an item
+
+
 @api_view(['PUT'])
 def update_profile(request, pk):
     data = request.data
     profile = Profile.objects.get(pk=pk)
-    serializer = ProfileSerializer(instance=profile, data=data)   # instance=profile is used to update the instance
+    # instance=profile is used to update the instance
+    serializer = ProfileSerializer(instance=profile, data=data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
+
 
 @api_view(['PUT'])
 def update_post(request, pk):
@@ -208,6 +228,7 @@ def update_post(request, pk):
         serializer.save()
         return Response(serializer.data)
 
+
 @api_view(['PUT'])
 def update_group(request, pk):
     data = request.data
@@ -217,6 +238,7 @@ def update_group(request, pk):
         serializer.save()
         return Response(serializer.data)
 
+
 @api_view(['PUT'])
 def update_comment(request, pk):
     data = request.data
@@ -225,15 +247,18 @@ def update_comment(request, pk):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-        
+
 # POST is used to create a new item
-@api_view(['POST'])  
+
+
+@api_view(['POST'])
 def create_post(request):
     data = request.data
     serializer = PostSerializer(data=data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data) 
+        return Response(serializer.data)
+
 
 @api_view(['POST'])
 def create_group(request):
@@ -243,13 +268,6 @@ def create_group(request):
         serializer.save()
         return Response(serializer.data)
 
-@api_view(['POST'])
-def create_comment(request):
-    data = request.data
-    serializer = CommentSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
 
 @api_view(['POST'])
 def create_comment(request):
@@ -260,4 +278,10 @@ def create_comment(request):
         return Response(serializer.data)
 
 
-
+@api_view(['POST'])
+def create_comment(request):
+    data = request.data
+    serializer = CommentSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
