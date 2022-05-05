e130873ab164d72d045da96f82a92307f666833e
Paul Munyao <paulmunyao094@gmail.com>
2022-05-05 15:27:32 +0300

    Add whitenoise to middleware in settings file

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
