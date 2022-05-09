b1f12242c9b5e943c346f36b5973632857c2fda7
Paul Munyao <paulmunyao094@gmail.com>
2022-05-09 15:20:49 +0300

    Creating user serializer

--- communication/serializers.py
+++ communication/serializers.py
@@ -23,3 +23,20 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+#User serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+#Register Serializer
+class RegisterSerializer(serializers.ModelSerializer): 
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')    
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
