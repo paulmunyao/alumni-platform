332ce8f4cdef4e63b2a5715b90c06852d70cf34e
Paul Munyao <paulmunyao094@gmail.com>
2022-05-10 06:28:57 +0300

    Set login api view

--- communication/views.py
+++ communication/views.py
@@ -310,3 +310,14 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI,self).post(request, format=None)
