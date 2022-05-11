8fec4b71190d65d9bb80fa51364d2f50b6e195ee
Paul Munyao <paulmunyao094@gmail.com>
2022-05-11 09:27:21 +0300

    Update models

--- communication/models.py
+++ communication/models.py
@@ -4,35 +4,48 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = CloudinaryField('image')
     bio = models.TextField(max_length=500, blank=True)
+
     def __str__(self):
         return self.user.username
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(max_length=500, blank=True)
-    updated = models.DateTimeField(auto_now=True)   
+    updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.user.username   
+        return self.user.username
+
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True)
     members = models.ManyToManyField(User, related_name='members')
+
     def __str__(self):
-        return self.name 
+        return self.name
+
+class Reply(models.Model):
+    reply = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.reply
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
     message = models.TextField(max_length=500, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.user.username
-        
-                        
