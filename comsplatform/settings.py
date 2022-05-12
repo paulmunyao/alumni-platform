f960e9769dd18bbd035bda0cfae2272d78d0a61d
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-05-12 21:37:12 +0300

    added a comma to installed apps

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -189,8 +189,6 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
