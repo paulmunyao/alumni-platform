5a3bafc6a58b2e0188fb8dc784e71bdef744f644
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 11:23:37 +0300

    set allowed hosts to true

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
