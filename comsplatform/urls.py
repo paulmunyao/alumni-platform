42f7fa05414cd7e2ff90450b07bcd40f676547a2
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:22:38 +0300

    change title

--- comsplatform/urls.py
+++ comsplatform/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Moringa Alumni Backend",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
