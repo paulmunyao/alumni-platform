985710634221344682e7be524c8c8af6793d0de3
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:25:57 +0300

    remove unused code line

--- comsplatform/urls.py
+++ comsplatform/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
     path('', include('communication.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger',
             cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
+    
 ]
