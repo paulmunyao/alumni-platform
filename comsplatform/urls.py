555528b03faec2aa4e9cb3ef311699a8092bbf21
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:26:27 +0300

    change path

--- comsplatform/urls.py
+++ comsplatform/urls.py
@@ -35,6 +35,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('communication.urls')),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+    path(r'^swagger/$', schema_view.with_ui('swagger',
             cache_timeout=0), name='schema-swagger-ui'),
 ]
