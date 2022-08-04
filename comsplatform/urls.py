cc55fbf78901e500fb26361b81aa5958f799a2eb
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:19:08 +0300

    install new dependancies to urls file

--- comsplatform/urls.py
+++ comsplatform/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
