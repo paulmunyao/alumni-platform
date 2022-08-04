8c27a435e4c67ee0c0879bda454dfba44c826fc2
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 11:24:51 +0300

    modify code

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -35,10 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
