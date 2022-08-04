1607d3edb176dd0cec3f2099bcfc76c0ed268a27
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:15:57 +0300

    indent code

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.staticfiles',  # required for serving swagger ui's css/js files
-   'drf_yasg',
+    'drf_yasg',
 ]
 
 ROOT_URLCONF = 'comsplatform.urls'
