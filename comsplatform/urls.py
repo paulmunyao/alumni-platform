41ac75d923f68112bce59e02d59f04c01f620278
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 12:19:50 +0300

    new code line

--- comsplatform/urls.py
+++ comsplatform/urls.py
@@ -19,6 +19,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('communication.urls')),
