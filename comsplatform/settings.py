3fb7b358e6b61c12f457699d7c8bd2c881d47bfc
paulmunyao <paulmunyao094@gmail.com>
2022-08-04 11:24:06 +0300

    modify settings

--- comsplatform/settings.py
+++ comsplatform/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
